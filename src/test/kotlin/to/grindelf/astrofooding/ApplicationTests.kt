1de0a3114832dd9ca29afa294604dd81711e996b
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-07-03 20:20:28 +0300

    initial

---
+++ src/test/kotlin/to/grindelf/astrofooding/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package to.grindelf.astrofooding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
